451ec856a4c5e431e6a97aa36e6e502003ab1c61
helols <helolsjava@gmail.com>
2009-12-11 18:19:29 +0000

    데스크탑 원복.
    git-svn-id: http://svn.pentome.com/isyoon/trunk@31 ea5790a9-a2b4-4b5f-b68c-306463eac401

--- web/js/desktop/Module.js
+++ web/js/desktop/Module.js
@@ -1,16 +1,15 @@
-Ext.ns('Ext.m31.Desktop');
-Desktop = function(cfg){
-    Ext.apply(this, cfg);
-    this.addEvents({
-        'ready' : true,
-        'beforeunload' : true
-    });
+/*!
+ * Ext JS Library 3.0.3
+ * Copyright(c) 2006-2009 Ext JS, LLC
+ * licensing@extjs.com
+ * http://www.extjs.com/license
+ */
+Ext.app.Module = function(config){
+    Ext.apply(this, config);
+    Ext.app.Module.superclass.constructor.call(this);
+    this.init();
+}
 
-    Ext.onReady(this.initDesktop, this);
-};
-Ext.extend(Desktop, Ext.util.Observable, {
-    isReady: false,
-    initDesktop : function(){
-        console.log('init!!');
-    }
+Ext.extend(Ext.app.Module, Ext.util.Observable, {
+    init : Ext.emptyFn
 });
