268731fcb5d16921c9296d8233b3f55c5f4dfa82
helols <helolsjava@gmail.com>
2009-12-17 09:19:20 +0000

    #99 이것도 클래스만 일단 작성함.
    git-svn-id: http://svn.pentome.com/isyoon/trunk@60 ea5790a9-a2b4-4b5f-b68c-306463eac401

---
+++ src/springsprout/m31/utils/OpenApiRequestHelper.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: helols
+ * Date: 2009. 12. 17
+ * Time: 오전 10:59:49
+ * enjoy springsprout ! development!
+ */
+package springsprout.m31.utils;
+
+public class OpenApiRequestHelper {
+        
+}
