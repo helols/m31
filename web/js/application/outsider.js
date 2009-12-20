57c20da3d90cfa8dc9343df3006320fbbd8eeff6
outsideris <outsideris@gmail.com>
2009-12-20 14:16:52 +0000

    새로운 createWindow로 수정중
    git-svn-id: http://svn.pentome.com/isyoon/trunk@77 ea5790a9-a2b4-4b5f-b68c-306463eac401

--- web/js/application/outsider.js
+++ web/js/application/outsider.js
@@ -3,87 +3,14 @@
  */
 
 // Spring See
-/*
 M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
-    id:'gallery-win',
-    init : function(){
-        this.launcher = {
-            text: 'Grid Window',
-            iconCls:'icon-grid',
-            handler : this.createWindow,
-            scope: this
-        };
-        
-        this.store = new Ext.data.JsonStore({
-		    url: '/sandbox/images', //this.config.url,
-		    root: 'images',
-		    fields: [
-		        'name', 'url',
-		        {name:'size', type: 'float'},
-		        {name:'lastmod', type:'date', dateFormat:'timestamp'}
-		    ]
-		});
-        this.store.load();
-        
-        this.tpl = new Ext.XTemplate(
-    		'<tpl for=".">',
-                '<div class="thumb-wrap" id="{name}">',
-    		    '<div class="thumb"><img src="{url}" title="{name}"></div>',
-    		    '<span class="x-editable">{shortName}</span></div>',
-            '</tpl>',
-            '<div class="x-clear"></div>'
-    	);
-    },
-
-    createWindow : function(){
-        var winManager = this.desktop.getWinManager();
-        var win = winManager.getWindow('grid-win');
-        this.store.load();
-        if(!win){
-            win = winManager.createWindow({
-                id: 'gallery-win',
-                title:'SpringSee Window',
-                width:740,
-                height:480,
-                iconCls: 'icon-grid',
-                shim:false,
-                animCollapse:false,
-                constrainHeader:true,
-
-                layout: 'fit',
-                items: new Ext.DataView({
-                    store: this.store,
-                    tpl: this.tpl,
-                    autoHeight:true,
-                    multiSelect: true,
-                    overClass:'x-view-over',
-                    itemSelector:'div.thumb-wrap',
-                    emptyText: 'No images to display',
-                    prepareData: function(data){
-                        data.shortName = Ext.util.Format.ellipsis(data.name, 15);
-                        data.sizeString = Ext.util.Format.fileSize(data.size);
-                        data.dateString = data.lastmod.format("m/d/Y g:i a");
-                        return data;
-                    },
-                    
-                    listeners: {
-                    	selectionchange: {
-                    		fn: function(dv,nodes){
-                    			var l = nodes.length;
-                    			var s = l != 1 ? 's' : '';
-                    			panel.setTitle('Simple DataView ('+l+' item'+s+' selected)');
-                    		}
-                    	}
-                    }
-                })
-            });
-        }
-        win.show();
-    }
-});
-*/
-
-M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
+	source : [
+	          ['all', "통합검색"],
+	          ['google', 'Google'],
+	          ['daum', 'Daum'],
+	          ['naver', 'Naver'],
+	          ['flickr', 'Flickr']
+	],
 	lookup : {},
 	init : function(){
 		console.log("init");
@@ -112,7 +39,7 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
 	            return (Math.round(((data.size*10) / 1024))/10) + " KB";
 	        }
 	    };
-        
+	    
         var formatData = function(data){
         	console.log("formatData");
 	    	data.shortName = data.name.ellipse(15);
@@ -121,7 +48,7 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
 	    	this.lookup[data.name] = data;
 	    	return data;
 	    };
-        
+	    
 	    this.view = new Ext.DataView({
 			tpl: this.thumbTemplate,
 			singleSelect: true,
@@ -131,107 +58,110 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
 			store: this.store,
 			listeners: {
 				'selectionchange': {fn:this.showDetails, scope:this, buffer:100},
-				'dblclick'       : {fn:this.doCallback, scope:this},
+				'dblclick'       : {fn:this.runGallery, scope:this},
 				'loadexception'  : {fn:this.onLoadException, scope:this},
 				'beforeselect'   : {fn:function(view){
-			        return view.store.getRange().length > 0;
+			        //return view.store.getRange().length > 0;
 			    }}
 			},
 			prepareData: formatData.createDelegate(this)
 		});
-        
+	    
+	  this.store.load();
+      //this.reset();
     },
 
-    createWindow : function(src){
-         var win = this.winManager.createWindow({
-            	title: 'Spring See',
-		    	layout: 'border',
-		    	width: 500,
-		    	height: 300,
-				minWidth: 500,
-				minHeight: 300,
-				closeAction: 'hide',
-				border: false,
-				items:[{
-					region: 'center',
-					autoScroll: true,
-					items: this.view,
-                    tbar:[{
-                    	text: 'Filter:'
-                    },{
-                    	xtype: 'textfield',
-                    	id: 'filter',
-                    	selectOnFocus: true,
-                    	width: 100,
-                    	listeners: {
-                    		'render': {fn:function(){
-						    	Ext.getCmp('filter').getEl().on('keyup', function(){
-						    		this.filter();
-						    	}, this, {buffer:500});
-                    		}, scope:this}
-                    	}
-                    }, ' ', '-', {
-                    	text: 'Sort By:'
-                    }, {
-                    	id: 'sortSelect',
-                    	xtype: 'combo',
-				        typeAhead: true,
-				        triggerAction: 'all',
-				        width: 100,
-				        editable: false,
-				        mode: 'local',
-				        displayField: 'desc',
-				        valueField: 'name',
-				        lazyInit: false,
-				        value: 'name',
-				        store: new Ext.data.ArrayStore({
-					        fields: ['name', 'desc'],
-					        data : [['name', 'Name'],['size', 'File Size'],['lastmod', 'Last Modified']]
-					    }),
-					    listeners: {
-							'select': {fn:this.sortImages, scope:this}
-					    }
-				    }]
-				},{
-					id: 'img-detail-panel',
-					region: 'east',
-					split: true,
-					width: 150,
-					minWidth: 150,
-					maxWidth: 250
-				}],
-				buttons: [{
-					id: 'ok-btn',
-					text: 'OK',
-					handler: this.doCallback,
-					scope: this
-				},{
-					text: 'Cancel',
-					handler: function(){ this.win.hide(); },
-					scope: this
-				}],
-				keys: {
-					key: 27, // Esc key
-					handler: function(){ this.win.hide(); },
-					scope: this
-				}
-            });
+    createWindow : {
+    	layout: 'border',
+    	width: 640,
+    	height: 480,
+		minWidth: 640,
+		minHeight: 480,
+		closeAction: 'hide',
+		border: false,
+		items:[{
+			region: 'center',
+			autoScroll: true,
+			items: this.view,
+            tbar:[{
+            	id: 'source',
+            	xtype: 'combo',
+            	store: new Ext.data.ArrayStore({
+                    fields: ['id', 'name'],
+                    data : this.source
+                }),
+                displayField:'name',
+                typeAhead: true,
+                width: 100,
+                mode: 'local',
+                editable: false,
+                displayField: 'name',
+                forceSelection: true,
+                lazyInit: false,
+                triggerAction: 'all',
+                value: '통합검색'
+//                        listeners: {
+//							'select': {fn:this.sortImages, scope:this}
+//					    }
+            },
+            {
+            	text: 'Search:'
+            },{
+            	xtype: 'textfield',
+            	id: 'search',
+            	selectOnFocus: true,
+            	width: 100,
+            	listeners: {
+            		'render': {fn:function(){
+				    	Ext.getCmp('search').getEl().on('keyup', function(){
+				    		this.filter();
+				    	}, this, {buffer:500});
+            		}, scope:this}
+            	}
+            }, ' ', '-', {
+            	text: 'Sort By:'
+            }, {
+            	id: 'sortSelect',
+            	xtype: 'combo',
+		        typeAhead: true,
+		        triggerAction: 'all',
+		        width: 100,
+		        editable: false,
+		        mode: 'local',
+		        displayField: 'desc',
+		        valueField: 'name',
+		        lazyInit: false,
+		        value: 'name',
+		        store: new Ext.data.ArrayStore({
+			        fields: ['name', 'desc'],
+			        data : [['name', 'Name'],['size', 'File Size'],['lastmod', 'Last Modified']]
+			    }),
+			    listeners: {
+					'select': {fn:this.sortImages, scope:this}
+			    }
+		    }, {
+		    	id: 'send',
+		    	xtype: 'button',
+		    	value: 'Send'
+		    }]
+		},{
+			id: 'img-detail-panel',
+			region: 'east',
+			split: true,
+			width: 150,
+			minWidth: 150,
+			maxWidth: 250
+		}],
+		keys: {
+			key: 27, // Esc key
+			handler: function(){ this.win.hide(); },
+			scope: this
+		}
 
-        console.log("before store.load");
-    	this.store.load();
-    	console.log("after store.load");
-    	this.reset(win);
-    	this.callback = function(data){
-        	console.log("callback called");
-    		Ext.DomHelper.append('images', {
-        		tag: 'img', src: data.url, style:'margin:10px;visibility:hidden;'
-        	}, true).show(true).frame();
-        	btn.focus();
-        };;
+    	//this.store.load();
+    	//this.reset(win);
         
-		//this.callback = callback;
-		//this.animateTarget = el;
-        return win;
+        //return win;
     },
     
     initTemplates : function(){
@@ -267,13 +197,11 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
 	    var detailEl = Ext.getCmp('img-detail-panel').body;
 		if(selNode && selNode.length > 0){
 			selNode = selNode[0];
-			Ext.getCmp('ok-btn').enable();
 		    var data = this.lookup[selNode.id];
             detailEl.hide();
             this.detailsTemplate.overwrite(detailEl, data);
             detailEl.slideIn('l', {stopFx:true,duration:.2});
 		}else{
-		    Ext.getCmp('ok-btn').disable();
 		    detailEl.update('');
 		}
 	},
@@ -294,30 +222,22 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
 
 	reset : function(arg){
     	console.log("reset");
-		if(arg.rendered){
-			Ext.getCmp('filter').reset();
-			this.view.getEl().dom.scrollTop = 0;
-		}
-	    this.view.store.clearFilter();
-		this.view.select(0);
+		
 	},
-
-	doCallback : function(){
-		console.log("doCallback");
-        var selNode = this.view.getSelectedNodes()[0];
-		var callback = this.callback;
-		var lookup = this.lookup;
-		win.hide(this.animateTarget, function(){
-            if(selNode && callback){
-				var data = lookup[selNode.id];
-				callback(data);
-			}
-		});
-    },
-
+	
 	onLoadException : function(v,o){
     	console.log("onLoadException");
 	    this.view.getEl().update('<div style="padding:10px;">Error loading images.</div>');
+	},
+	
+	//Gallery 실행
+	runGallery : function(){
+		alert("run Gallery")
+    },
+	
+	// 검색 소스 고르기
+	selectSource : function() {
+    	alert(Ext.getCmp('source').getValue());
 	}
 });
 
