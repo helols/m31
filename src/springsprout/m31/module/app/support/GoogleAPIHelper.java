2653a28c689fca9ae3bfdd3093867fad3a1c465b
outsideris <outsideris@gmail.com>
2009-12-25 15:27:36 +0000

    #95 구글 이미지 검색 연결
    git-svn-id: http://svn.pentome.com/isyoon/trunk@123 ea5790a9-a2b4-4b5f-b68c-306463eac401

--- src/springsprout/m31/module/app/support/GoogleAPIHelper.java
+++ src/springsprout/m31/module/app/support/GoogleAPIHelper.java
@@ -8,28 +8,31 @@
 package springsprout.m31.module.app.support;
 
 import springsprout.m31.dto.SpringseeDTO;
+import springsprout.m31.utils.JSONHelper;
+import springsprout.m31.utils.OpenApiRequestHelper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GoogleAPIHelper {
     public static Integer springsee(ArrayList<SpringseeDTO> r_list, String[] apiInfo, String query, Integer pageNo, Integer perPage) {
     	String api_url = apiInfo[0]; //
         String api_output = apiInfo[1];
+        api_url += query+"&&rsz=large";
+        api_url += "&start="+pageNo;
         System.out.println("Google api : " + api_url);
-//        api_url += query+"&output="+api_output.toLowerCase();
-//        api_url += "&sort=1&pageno="+pageNo + "&result="+perPage;
-//        
-//        ArrayList<HashMap<String, String>> tmpList = JSONHelper.jsonArrayConverToArrayList(OpenApiRequestHelper.loadString(api_url),"channel.item");
-//        for(HashMap<String, String> tmpMap : tmpList){
-//            r_list.add(
-//                    new SpringseeDTO(
-//                            tmpMap.get("thumbnail")
-//                           ,tmpMap.get("width")
-//                           ,tmpMap.get("height")
-//                           ,tmpMap.get("title")
-//                           ,tmpMap.get("link"))
-//            );
-//        }
+
+        ArrayList<HashMap<String, Object>> tmpList = JSONHelper.jsonArrayConverToArrayList(OpenApiRequestHelper.loadString(api_url),"responseData.results");
+        for(HashMap<String, Object> tmpMap : tmpList){
+            r_list.add(
+                    new SpringseeDTO(
+                    		tmpMap.get("tbUrl").toString()
+                           ,tmpMap.get("width").toString()
+                           ,tmpMap.get("height").toString()
+                           ,tmpMap.get("titleNoFormatting").toString()
+                           ,tmpMap.get("url").toString())
+            );
+        }
         return 0;
     }
 }
