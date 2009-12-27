6a9395805b83ada7ccee427180b8c654a6356be1
helols <helolsjava@gmail.com>
2009-12-27 11:06:52 +0000

    주석 보강.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    git-svn-id: http://svn.pentome.com/isyoon/trunk@132 ea5790a9-a2b4-4b5f-b68c-306463eac401

--- src/springsprout/m31/utils/JSONHelper.java
+++ src/springsprout/m31/utils/JSONHelper.java
@@ -20,8 +20,8 @@ public class JSONHelper {
 
     /**
      * 넘겨는 listNAme 의로 list를 json string 으로 변환해준다.
-     * @param list
-     * @param listName
+     * @param list 변환대상 list
+     * @param listName root name
      * @return json 형식의 String
      */
     public static String listToJSONstr(List<?> list, String listName) {
@@ -35,7 +35,7 @@ public class JSONHelper {
 
     /**
      * 해당 도메인 객체를 Json string 으로 변환해준다.
-     * @param obj
+     * @param obj 변환 도메인 객체
      * @return json 형식의 String
      */
     public static String domainToJSONstr(Object obj) {
@@ -43,7 +43,7 @@ public class JSONHelper {
     }
     /**
      * map 안에 담겨 있는 단일 object를 json String으로 변환해준다.
-     * @param map
+     * @param map 변환할 MAP
      * @return json 형식의 String
      */
 
@@ -59,7 +59,7 @@ public class JSONHelper {
 
     /**
      * map 안에 담겨 있는 array || list를 json String 으로 변환해준다.
-     * @param map
+     * @param map 변환할 MAP
      * @return
      */
     public static String mapInListToJSONstr(Map map) {
@@ -72,7 +72,7 @@ public class JSONHelper {
 
     /**
      * 넘겨주는 문자열을 HashMap으로 돌려준다.
-     * @param jsonString
+     * @param jsonString HashMap으로 담을 jsonSring.
      * @return HashMap
      */
     @SuppressWarnings("uncheched")
@@ -83,8 +83,9 @@ public class JSONHelper {
     /**
      * path에 해당하는 json array를 ArrayList안에 hashmap을 넣어서 반환한다.
      * @see JSONHelperTest.jsonArrayConverToArrayListTest
-     * @param jsonString
-     * @param path
+     * @see springsprout.m31.module.app.support.DaumAPIHelper.springsee() 
+     * @param jsonString  변환대상 json spring.
+     * @param path jsonstring에서 변환을 시작할 array name. (ex: channel.item); ex value. "{\"channel\":{\"itm\":[{\"thumbnail\":\"http://image02.search.daum-img.net/03/0.c3.b6.BL_shopping-how_472_0.jpg\",\"width\":\"520\",\"height\":\"347\",\"cp\":\"7\",\"title\":\"[Daum Life Changers Shopping7]쇼핑하우를 IPTV 속으로 넣어보자!!\",\"image\":\"\",\"link\":\"http://blog.daum.net/shopping-how/472\",\"pubDate\":\"20091203212840\",\"author\":\"8AFQI\"},{\"thumbnail\":\"http://image02.search.daum-img.net/01/f.e7.8a.TI_460923862_0.jpg\",\"width\":\"750\",\"height\":\"474\",\"cp\":\"7\",\"title\":\"Daum검색\",\"image\":\"\",\"link\":\"http://lucero.tistory.com/3\",\"pubDate\":\"20090826170811\",\"author\":\"jelldis@hanmail.net\"},{\"thumbnail\":\"http://image02.search.daum-img.net/01/8.8e.e7.TI_460921295_0.jpg\",\"width\":\"893\",\"height\":\"595\",\"cp\":\"7\",\"title\":\"Daum카페\",\"image\":\"\",\"link\":\"http://lucero.tistory.com/1\",\"pubDate\":\"20090826165238\",\"author\":\"jelldis@hanmail.net\"},{\"thumbnail\":\"http://image02.search.daum-img.net/02/a.a8.a2.BL_aleedsun_39197_1.jpg\",\"width\":\"899\",\"height\":\"696\",\"cp\":\"7\",\"title\":\"절규\",\"image\":\"\",\"link\":\"http://blog.daum.net/aleedsun/39197\",\"pubDate\":\"20090601203220\",\"author\":\"pOPc\"},{\"thumbnail\":\"http://image02.search.daum-img.net/03/b.8e.06.TI_apreview.tistory.com_7_4.jpg\",\"width\":\"320\",\"height\":\"480\",\"cp\":\"7\",\"title\":\"Daum 지도\",\"image\":\"\",\"link\":\"http://tsunamis.tistory.com/72\",\"pubDate\":\"20090427135916\",\"author\":\"psw7574@naver.com\"},{\"thumbnail\":\"http://image02.search.daum-img.net/02/1.ff.97.TI_tsunamis.tistory.com_72_4.jpg\",\"width\":\"320\",\"height\":\"480\",\"cp\":\"7\",\"title\":\"Daum 지도\",\"image\":\"\",\"link\":\"http://tsunamis.tistory.com/72\",\"pubDate\":\"20090427135916\",\"author\":\"psw7574@naver.com\"},{\"thumbnail\":\"http://image02.search.daum-img.net/02/f.57.4b.TI_imchool.tistory.com_114_0.jpg\",\"width\":\"320\",\"height\":\"480\",\"cp\":\"7\",\"title\":\"Daum 지도\",\"image\":\"\",\"link\":\"http://imchool.tistory.com/114\",\"pubDate\":\"20090426231711\",\"author\":\"imchool@gmail.com\"},{\"thumbnail\":\"http://image02.search.daum-img.net/02/d.a2.de.TI_176500_4_0.jpg\",\"width\":\"1024\",\"height\":\"768\",\"cp\":\"7\",\"title\":\"Daum♡\",\"image\":\"\",\"link\":\"http://risyana.tistory.com/4\",\"pubDate\":\"20090208024841\",\"author\":\"risyana@hanmail.net\"},{\"thumbnail\":\"http://image02.search.daum-img.net/01/f.94.7f.BL_0MdNC_84_1.jpg\",\"width\":\"760\",\"height\":\"315\",\"cp\":\"15\",\"title\":\"daum 지도\",\"image\":\"\",\"link\":\"http://cafe.daum.net/HKKOREAN/4qWC/320?docid=1Db7e|4qWC|320|20090124125921\",\"pubDate\":\"20090124125921\",\"author\":\"4BVxc\"},{\"thumbnail\":\"http://image02.search.daum-img.net/03/5.69.b0.CF_1Fpgm_PntS_88_2.jpg\",\"width\":\"600\",\"height\":\"471\",\"cp\":\"15\",\"title\":\"daumpc\",\"image\":\"\",\"link\":\"http://cafe.daum.net/DaumPC/PntS/88?docid=1Fpgm|PntS|88|20081224212859\",\"pubDate\":\"20081224212859\",\"author\":\"4onQk\"}],\"sort\":\"1\",\"title\":\"Search image Daum Open API\",\"result\":\"10\",\"totalCount\":51876,\"pageno\":\"1\",\"q\":\"daum\",\"desc\":\"Daum Open API search result\"}}"; 이중에서 item 부분을 받고 싶을 경우에 해당함.
      * @return
      */
     @SuppressWarnings("unchecked")
