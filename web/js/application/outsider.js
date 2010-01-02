ccdd05e685594093f06948280b2f594ff380cd29
outsideris <outsideris@gmail.com>
2010-01-02 16:37:01 +0000

    봄씨 드래그앤드롭 테스트중
    git-svn-id: http://svn.pentome.com/isyoon/trunk@174 ea5790a9-a2b4-4b5f-b68c-306463eac401

--- web/js/application/outsider.js
+++ web/js/application/outsider.js
@@ -12,7 +12,6 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
         ['flickr', 'Flickr']
     ],
     currentPage:1,
-    loadMask : null, 
     lookup : {},
     init : function() {
         //필요한 JS
@@ -41,8 +40,10 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
                 'load': {fn:function() {
                     console.log("store loaded");
                     this.view.select(0);
-                    m31.showImage();
-                }, scope:this, single:true}
+                    //m31.showImage();
+                    this.dragZone = new ImageDragZone(this.view, {containerScroll:true, ddGroup: 'explorerDD'});
+                    //initializePatientDragZone(Ext.get('springsee-view').select('div.thumb'));
+                }, scope:this, single:false}
             }
         });
 
@@ -71,7 +72,9 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
             singleSelect: true,
             overClass:'x-view-over',
             itemSelector: 'div.thumb-wrap',
+            multiSelect: true,
             emptyText : '<div style="padding:10px;">No images match the specified search</div>',
+            plugins: new Ext.DataView.DragSelector({dragSafe:true}),
             store: this.store,
             listeners: {
                 //'selectionchange': {fn:this.showDetails, scope:this, buffer:100},
@@ -84,7 +87,7 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
             prepareData: formatData.createDelegate(this)
         });
     },
-
+    
     /**
      *  해당 app의 window가 제거 될때 호출되는 콜백.
      */
@@ -109,6 +112,7 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
         if(!this.win){
             this.win = win;
         }
+        Ext.getCmp('springsee-explorer-panel').collapse();
     },
 
     createWindow : function () {
@@ -126,7 +130,6 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
                     id: 'springsee-view',
                     region: 'center',
                     autoScroll: true,
-                    loadMask: {msg:'Loading Feed...'},
                     items: this.view,
                     tbar:[
                         {
@@ -153,9 +156,7 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
                         },
                         ' ',
                         '-',
-                        {
-                            text: 'Search:'
-                        },
+                        'Search:',
                         {
                             xtype: 'textfield',
                             id: 'springsee-search',
@@ -204,6 +205,26 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
                             scope: this
                         }
                     ]
+                },
+                {
+                    region: 'south',
+                    title: '탐색기',
+                    xtype: 'panel',
+                    collapsedTitle: '탐색기',
+                    id: 'springsee-explorer-panel',
+                    autoScroll: true,
+                    collapsible: true,
+                    ddGroup: 'explorerDD',
+                    split: true,
+                    margins: '0 0 0 0',
+                    cmargins: '2 2 2 2',
+                    height: 100,
+                    html: "Drag Images to here...",
+                    listeners: {
+                		'render': {fn:function() {
+                            //initializeHospitalDropZone(this.view);
+                        }, scope:this}
+                	}
                 }
             ]
         };
@@ -281,6 +302,9 @@ String.prototype.ellipse = function(maxLength) {
 };
 
 m31.showImage = function() {
+	$(".piro_overlay").remove();
+	$(".pirobox_content").remove();
+	
 	$().piroBox({
 		my_speed: 600, //animation speed
 		bg_alpha: 0.5, //background opacity
@@ -292,4 +316,181 @@ m31.showImage = function() {
 		slideShow : 'slideshow', // just delete slideshow between '' if you don't want it.
 		slideSpeed : 4 //slideshow duration in seconds(3 to 6 Recommended)
 	});
+};
+
+/**
+ * Create a DragZone instance for our JsonView
+ */
+ImageDragZone = function(view, config){
+    this.view = view;
+    ImageDragZone.superclass.constructor.call(this, view.getEl(), config);
+};
+Ext.extend(ImageDragZone, Ext.dd.DragZone, {
+    // We don't want to register our image elements, so let's 
+    // override the default registry lookup to fetch the image 
+    // from the event instead
+    getDragData : function(e){
+		console.log("call getDragData");
+        var target = e.getTarget('.thumb-wrap');
+        if(target){
+            var view = this.view;
+            if(!view.isSelected(target)){
+                view.onClick(e);
+            }
+            var selNodes = view.getSelectedNodes();
+            var dragData = {
+                nodes: selNodes
+            };
+            if(selNodes.length == 1){
+                dragData.ddel = target;
+                dragData.single = true;
+            }else{
+                var div = document.createElement('div'); // create the multi element drag "ghost"
+                div.className = 'multi-proxy';
+                for(var i = 0, len = selNodes.length; i < len; i++){
+                    div.appendChild(selNodes[i].firstChild.firstChild.cloneNode(true)); // image nodes only
+                    if((i+1) % 3 == 0){
+                        div.appendChild(document.createElement('br'));
+                    }
+                }
+                var count = document.createElement('div'); // selected image count
+                count.innerHTML = i + ' images selected';
+                div.appendChild(count);
+                
+                dragData.ddel = div;
+                dragData.multi = true;
+            }
+            return dragData;
+        }
+        return false;
+    },
+
+    // this method is called by the TreeDropZone after a node drop
+    // to get the new tree node (there are also other way, but this is easiest)
+    getTreeNode : function(){
+        var treeNodes = [];
+        var nodeData = this.view.getRecords(this.dragData.nodes);
+        for(var i = 0, len = nodeData.length; i < len; i++){
+            var data = nodeData[i].data;
+            treeNodes.push(new Ext.tree.TreeNode({
+                text: data.name,
+                icon: '../view/'+data.url,
+                data: data,
+                leaf:true,
+                cls: 'image-node'
+            }));
+        }
+        return treeNodes;
+    },
+    
+    // the default action is to "highlight" after a bad drop
+    // but since an image can't be highlighted, let's frame it 
+    afterRepair:function(){
+        for(var i = 0, len = this.dragData.nodes.length; i < len; i++){
+            Ext.fly(this.dragData.nodes[i]).frame('#8db2e3', 1);
+        }
+        this.dragging = false;    
+    },
+    
+    // override the default repairXY with one offset for the margins and padding
+    getRepairXY : function(e){
+        if(!this.dragData.multi){
+            var xy = Ext.Element.fly(this.dragData.ddel).getXY();
+            xy[0]+=3;xy[1]+=3;
+            return xy;
+        }
+        return false;
+    }
+});
+
+Ext.DataView.DragSelector = function(cfg){
+	console.log("DragSelector");
+    cfg = cfg || {};
+    var view, proxy, tracker;
+    var rs, bodyRegion, dragRegion = new Ext.lib.Region(0,0,0,0);
+    var dragSafe = cfg.dragSafe === true;
+
+    this.init = function(dataView){
+        view = dataView;
+        view.on('render', onRender);
+    };
+
+    function fillRegions(){
+        rs = [];
+        view.all.each(function(el){
+            rs[rs.length] = el.getRegion();
+        });
+        bodyRegion = view.el.getRegion();
+    }
+
+    function cancelClick(){
+        return false;
+    }
+
+    function onBeforeStart(e){
+        return !dragSafe || e.target == view.el.dom;
+    }
+
+    function onStart(e){
+    	console.log("onStart");
+        view.on('containerclick', cancelClick, view, {single:true});
+        if(!proxy){
+            proxy = view.el.createChild({cls:'x-view-selector'});
+        }else{
+            if(proxy.dom.parentNode !== view.el.dom){
+                view.el.dom.appendChild(proxy.dom);
+            }
+            proxy.setDisplayed('block');
+        }
+        fillRegions();
+        view.clearSelections();
+    }
+
+    function onDrag(e){
+        var startXY = tracker.startXY;
+        var xy = tracker.getXY();
+
+        var x = Math.min(startXY[0], xy[0]);
+        var y = Math.min(startXY[1], xy[1]);
+        var w = Math.abs(startXY[0] - xy[0]);
+        var h = Math.abs(startXY[1] - xy[1]);
+
+        dragRegion.left = x;
+        dragRegion.top = y;
+        dragRegion.right = x+w;
+        dragRegion.bottom = y+h;
+
+        dragRegion.constrainTo(bodyRegion);
+        proxy.setRegion(dragRegion);
+
+        for(var i = 0, len = rs.length; i < len; i++){
+            var r = rs[i], sel = dragRegion.intersect(r);
+            if(sel && !r.selected){
+                r.selected = true;
+                view.select(i, true);
+            }else if(!sel && r.selected){
+                r.selected = false;
+                view.deselect(i);
+            }
+        }
+    }
+
+    function onEnd(e){
+        if (!Ext.isIE) {
+            view.un('containerclick', cancelClick, view);    
+        }        
+        if(proxy){
+            proxy.setDisplayed(false);
+        }
+    }
+
+    function onRender(view){
+        tracker = new Ext.dd.DragTracker({
+            onBeforeStart: onBeforeStart,
+            onStart: onStart,
+            onDrag: onDrag,
+            onEnd: onEnd
+        });
+        tracker.initEl(view.el);
+    }
 };
