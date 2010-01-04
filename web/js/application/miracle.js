4f6e400c16df51016215fc391760349042d7b14c
miracle0k <kmiracle@gmail.com>
2010-01-04 12:56:56 +0000

    #122 봄플레이어 검색 UI 제작
    window 닫은 후 다시 안열리는 문제 해결.
    
    git-svn-id: http://svn.pentome.com/isyoon/trunk@197 ea5790a9-a2b4-4b5f-b68c-306463eac401

--- web/js/application/miracle.js
+++ web/js/application/miracle.js
@@ -4,65 +4,66 @@ M31Desktop.SpringPlayer = Ext.extend(M31.app.Module, {
      */
     init : function() {
         console.log("Init Call.....");
-        // 스토어
-        this.ds = new Ext.data.JsonStore({
-            url: '/gateway/springplayer/search',
-            restful : true,
-            root: 'items'
-        });
-
-        // 동영상 검색 부분
-        this.serchePanel = new Ext.Panel({
-            id : 'springPlayer-serach',
-            layout : 'border',
-
-            items: [{
-                region : 'center',
-                tbar : [
-                    //  검색 콤보 박스
-                    {
-                        id: 'springplayer-serach-combo',
-                        xtype: 'combo',
-                        store: new Ext.data.ArrayStore({
-                            fields: ['id', 'name'],
-                            data : [['google', 'Google'],['daum', 'Daum']]
-                        }),
-                        displayField:'name',
-                        typeAhead: true,
-                        width: 100,
-                        mode: 'local',
-                        editable: false,
-                        displayField: 'name',
-                        valueField: 'id',
-                        forceSelection: true,
-                        lazyInit: false,
-                        triggerAction: 'all',
-                        value: 'Google'
-                    }, '-',
-                    // 검색 필드
-                    new Ext.app.SearchField({
-                       width:200,
-                       store : this.ds
-                    })
-                ],
-
-                bbar : ['test']
-            }, {
-                title : '탐색기',
-                region : 'south',
-                height : 100,
-                html : '<b>south</b>',
-                collapsible: true,
-                split: true
-            }]
-        });
     },
     createCallback : function(win) {
        console.log("Create Call Back.....");
+        if(win == undefined) {
+
+        }
     },
     beforeCreate : function(){
         console.log("beforeCreate");
-        
+        // 스토어
+         this.ds = new Ext.data.JsonStore({
+             url: '/gateway/springplayer/search',
+             restful : true,
+             root: 'items'
+         });
+
+         // 동영상 검색 부분
+         this.serchePanel = new Ext.Panel({
+             id : 'springPlayer-serach',
+             layout : 'border',
+
+             items: [{
+                 region : 'center',
+                 tbar : [
+                     //  검색 콤보 박스
+                     {
+                         id: 'springplayer-serach-combo',
+                         xtype: 'combo',
+                         store: new Ext.data.ArrayStore({
+                             fields: ['id', 'name'],
+                             data : [['google', 'Google'],['daum', 'Daum']]
+                         }),
+                         typeAhead: true,
+                         width: 100,
+                         mode: 'local',
+                         editable: false,
+                         displayField: 'name',
+                         valueField: 'id',
+                         forceSelection: true,
+                         lazyInit: false,
+                         triggerAction: 'all',
+                         value: 'Google'
+                     }, '-',
+                     // 검색 필드
+                     new Ext.app.SearchField({
+                        width:200,
+                        store : this.ds
+                     })
+                 ],
+
+                 bbar : ['test']
+             }, {
+                 title : '탐색기',
+                 region : 'south',
+                 height : 100,
+                 html : '<strong>south</strong>',
+                 collapsible: true,
+                 split: true
+             }]
+         });
     },
     removeWin: function(){
         console.log("removeWin");
@@ -70,26 +71,28 @@ M31Desktop.SpringPlayer = Ext.extend(M31.app.Module, {
     },
     createWindow : function() {
         console.log("CreateWindow");
-        return {
-            id : 'springplayer',
-            title : 'Spring Player',
-            width: 640,
-            height: 480,
-            minWidth: 640,
-            minHeight: 480,
 
-            layout : 'card',            
-            activeItem : 0,
+        var config = {
+                id : 'springplayer',
+                title : 'Spring Player',
+                width: 640,
+                height: 480,
+                minWidth: 640,
+                minHeight: 480,
 
-            items : [ this.serchePanel,
-            {
-                id : 'springPlayer-player',
-                tbar : ["Title"],
-                html : '<h1>This is Player Panel</h1>'
-            }]
+                layout : 'card',
+                activeItem : 0,
+
+                items : [ this.serchePanel,
+                {
+                    id : 'springPlayer-player',
+                    tbar : ["Title"],
+                    html : '<h1>This is Player Panel</h1>'
+                }]
         };
+        
+        return config;
     }
-    /* 내가 추가한 함수나 변수 */
 });
 
 /*검색 필드*/
