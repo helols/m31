091523fb4de72fd035e006ab9751b5d1232f904e
helols <helolsjava@gmail.com>
2010-01-10 14:09:41 +0000

    트위터 관련 어시스트.
    git-svn-id: http://svn.pentome.com/isyoon/trunk@259 ea5790a9-a2b4-4b5f-b68c-306463eac401

--- src/springsprout/m31/module/app/twitter/SpringTwitterAuthorization.java
+++ src/springsprout/m31/module/app/twitter/SpringTwitterAuthorization.java
@@ -3,7 +3,9 @@ package springsprout.m31.module.app.twitter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import springsprout.m31.common.OpenApiReadException;
 import springsprout.m31.module.app.twitter.support.TwitterAuthorizationDTO;
 import springsprout.m31.service.security.SecurityService;
 import twitter4j.Twitter;
@@ -11,73 +13,83 @@ import twitter4j.TwitterException;
 import twitter4j.http.AccessToken;
 import twitter4j.http.RequestToken;
 
+@Service
+@Transactional
 public class SpringTwitterAuthorization {
-	
-	Logger log = LoggerFactory.getLogger(getClass());
-	
-	@Autowired SpringTwitterRepository twitterRepository;
-	
-	private final String CONSUMER_KEY = "gwf70oR6DN1J0jNQmGeSQ";
-	private final String CONSUMER_SECRET = "4cYniE2TlosIqplbQGcmkcMtjLG2bFKCcrifemoIu2Q";
-	
-	public Boolean checkAuthorization(SecurityService securityService) throws TwitterException {
-		String userToken;
-		String userSecretToken;
-		
-		TwitterAuthorizationDTO twitterAuthDTO = twitterRepository.getUserAuthTokenByMemberId(securityService.getCurrentMemberId());
-		userToken = twitterAuthDTO.getToken();
-		userSecretToken = twitterAuthDTO.getSecret_token();
-		
-		if (userToken.trim().equals("") || userToken == null || userSecretToken.trim().equals("") || userSecretToken == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-	public String getAuthorizationURL(SecurityService securityService) throws TwitterException {
-		Twitter twitter = new Twitter();
-		twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-		RequestToken requestToken = twitter.getOAuthRequestToken();
-		
+
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    SpringTwitterRepository twitterRepository;
+    @Autowired
+    SecurityService securityService;
+
+    private final String CONSUMER_KEY = "gwf70oR6DN1J0jNQmGeSQ";
+    private final String CONSUMER_SECRET = "4cYniE2TlosIqplbQGcmkcMtjLG2bFKCcrifemoIu2Q";
+
+    public Boolean checkAuthorization() {
+        String userToken;
+        String userSecretToken;
+
+        TwitterAuthorizationDTO twitterAuthDTO = twitterRepository.getUserAuthTokenByMemberId(securityService.getCurrentMemberId());
+        userToken = twitterAuthDTO.getToken();
+        userSecretToken = twitterAuthDTO.getSecret_token();
+
+        if (userToken.trim().equals("") || userToken == null || userSecretToken.trim().equals("") || userSecretToken == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public String getAuthorizationURL() {
+        Twitter twitter = new Twitter();
+        twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+        RequestToken requestToken = null;
+        try {
+            requestToken = twitter.getOAuthRequestToken();
+        } catch (TwitterException e) {
+            throw new OpenApiReadException(e);
+        }
+
 //		String userRequestToken = "mNFFhBcIvYSxvHg82QNImEsrihexfriXbUiUd3gM";
 //		String userSecretToken = "P0sILcVd5ERJpeiIeJQrXAEmIQ7mqggC5134ATIM";
-		TwitterAuthorizationDTO authDTO = new TwitterAuthorizationDTO();
-		authDTO.setToken(requestToken.getToken());
-		authDTO.setSecret_token(requestToken.getTokenSecret());
-		
-		securityService.setTwitterAuthorizationToken(authDTO);
-		
-		log.debug("redirect URL before Auth>>>" + requestToken.getAuthorizationURL());
-		log.debug("token before Auth>>>" + requestToken.getToken());
-		log.debug("secrettoken before Auth>>>" + requestToken.getTokenSecret());
+        TwitterAuthorizationDTO authDTO = new TwitterAuthorizationDTO();
+        authDTO.setToken(requestToken.getToken());
+        authDTO.setSecret_token(requestToken.getTokenSecret());
 
-		return requestToken.getAuthorizationURL();
-	}
-	
-	public void getAuthorization(SecurityService securityService) throws TwitterException {
+        securityService.setTwitterAuthorizationToken(authDTO);
+
+        log.debug("redirect URL before Auth>>>" + requestToken.getAuthorizationURL());
+        log.debug("token before Auth>>>" + requestToken.getToken());
+        log.debug("secrettoken before Auth>>>" + requestToken.getTokenSecret());
+
+        return requestToken.getAuthorizationURL();
+    }
+
+    public void getAuthorization() {
 //		String userToken;
 //		String userSecretToken;
-		
-		TwitterAuthorizationDTO twitterAuthDTO = new TwitterAuthorizationDTO();
-		twitterAuthDTO = twitterRepository.getUserAuthTokenByMemberId(securityService.getCurrentMemberId());
-		//테스트용 Token, Outsider계정
+
+        TwitterAuthorizationDTO twitterAuthDTO = new TwitterAuthorizationDTO();
+        twitterAuthDTO = twitterRepository.getUserAuthTokenByMemberId(securityService.getCurrentMemberId());
+        //테스트용 Token, Outsider계정
 //		userToken = "7932892-t1WhGYBoUbuufGXqMP7F1H4xIvYDQFilSGZeZcJTbn";
 //		userSecretToken = "ukpHS7Kw9DKDQxTvF76vehXxZNUenZ8rixxUyw7ANw";
 //		userSecretToken = "Gut23fNxEsh60lpQNx46XiYg5iQH9h5zFEOecq5Oqc";
-		
-		Twitter twitter = new Twitter();
-		twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-		
-		AccessToken accessToken = null;
-		accessToken = new AccessToken(twitterAuthDTO.getToken(), twitterAuthDTO.getSecret_token());
-		twitter.setOAuthAccessToken(accessToken);
-		
-		securityService.setTwitterObject(twitter);
-		
-		log.debug("token after Auth>>>" + accessToken.getToken());
-		log.debug("secrettoken after Auth>>>" + accessToken.getTokenSecret());
-		
+
+        Twitter twitter = new Twitter();
+        twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+
+        AccessToken accessToken = null;
+        accessToken = new AccessToken(twitterAuthDTO.getToken(), twitterAuthDTO.getSecret_token());
+        twitter.setOAuthAccessToken(accessToken);
+
+        securityService.setTwitterObject(twitter);
+
+        log.debug("token after Auth>>>" + accessToken.getToken());
+        log.debug("secrettoken after Auth>>>" + accessToken.getTokenSecret());
+
 //		int id = accessToken.getUserId();
 //		User user = twitter.showUser(id+"");
 //		String screenName = user.getScreenName();
@@ -90,48 +102,53 @@ public class SpringTwitterAuthorization {
 //	        System.out.println(status.getUser().getName() + ":" +
 //	                           status.getText());
 //	    }
-	}
-	
-	public Boolean storeAuthToken(String oauthToken, SecurityService securityService) throws TwitterException {
-		Twitter twitter = new Twitter();
-		twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-		
-		TwitterAuthorizationDTO twitterAuthDTO = new TwitterAuthorizationDTO();
-		securityService.getTwitterAuthorizationToken();
-		
-		twitterAuthDTO = securityService.getTwitterAuthorizationToken();;
-		
-		AccessToken accessToken = null;
-		
-		log.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-		log.debug("token from Session>>>" + twitterAuthDTO.getToken());
-		log.debug("stoken from Session>>>" + twitterAuthDTO.getSecret_token());
-		log.debug("receive Token from Twitter>>>" + oauthToken);
-		
-		if (twitterAuthDTO.getToken().equals(oauthToken)) {
-			log.debug("same Token");
-			accessToken = twitter.getOAuthAccessToken(oauthToken, twitterAuthDTO.getSecret_token());
-			twitter.setOAuthAccessToken(accessToken);
-			
-			// persist token
-			TwitterAuthorizationDTO authDTO = new TwitterAuthorizationDTO();
-			authDTO.setMember_id(securityService.getCurrentMemberId());
-			authDTO.setToken(accessToken.getToken());
-			authDTO.setSecret_token(accessToken.getTokenSecret());
-			twitterRepository.insertUserAuthToken(authDTO);
-			
-			accessToken = new AccessToken(accessToken.getToken(), accessToken.getTokenSecret());
-			twitter.setOAuthAccessToken(accessToken);
-			
-			log.debug("auth token>>>" + accessToken.getToken());
-			log.debug("auth secrettoken>>>" + accessToken.getTokenSecret());
-			
-			securityService.setTwitterObject(twitter);
-			
-			return true;
-		} else {
-			log.debug("request token is not same.");
-			return false;
-		}
-	}
+    }
+
+    public Boolean storeAuthToken(String oauthToken) {
+        Twitter twitter = new Twitter();
+        twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+
+        TwitterAuthorizationDTO twitterAuthDTO = new TwitterAuthorizationDTO();
+        securityService.getTwitterAuthorizationToken();
+
+        twitterAuthDTO = securityService.getTwitterAuthorizationToken();
+        ;
+
+        AccessToken accessToken = null;
+
+        log.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        log.debug("token from Session>>>" + twitterAuthDTO.getToken());
+        log.debug("stoken from Session>>>" + twitterAuthDTO.getSecret_token());
+        log.debug("receive Token from Twitter>>>" + oauthToken);
+
+        if (twitterAuthDTO.getToken().equals(oauthToken)) {
+            log.debug("same Token");
+            try {
+                accessToken = twitter.getOAuthAccessToken(oauthToken, twitterAuthDTO.getSecret_token());
+            } catch (TwitterException e) {
+                throw new OpenApiReadException(e);
+            }
+            twitter.setOAuthAccessToken(accessToken);
+
+            // persist token
+            TwitterAuthorizationDTO authDTO = new TwitterAuthorizationDTO();
+            authDTO.setMember_id(securityService.getCurrentMemberId());
+            authDTO.setToken(accessToken.getToken());
+            authDTO.setSecret_token(accessToken.getTokenSecret());
+            twitterRepository.insertUserAuthToken(authDTO);
+
+            accessToken = new AccessToken(accessToken.getToken(), accessToken.getTokenSecret());
+            twitter.setOAuthAccessToken(accessToken);
+
+            log.debug("auth token>>>" + accessToken.getToken());
+            log.debug("auth secrettoken>>>" + accessToken.getTokenSecret());
+
+            securityService.setTwitterObject(twitter);
+
+            return true;
+        } else {
+            log.debug("request token is not same.");
+            return false;
+        }
+    }
 }
--- src/springsprout/m31/module/app/twitter/SpringTwitterController.java
+++ src/springsprout/m31/module/app/twitter/SpringTwitterController.java
@@ -1,9 +1,5 @@
 package springsprout.m31.module.app.twitter;
 
-import static springsprout.m31.common.M31System.JSON_VIEW;
-
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import springsprout.m31.module.app.twitter.support.TwitterAuthorizationDTO;
-import springsprout.m31.service.security.SecurityService;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
+import static springsprout.m31.common.M31System.JSON_VIEW;
 
 /*
  * 
@@ -35,49 +28,39 @@ import twitter4j.TwitterException;
  */
 
 @Controller
-@RequestMapping(value="/app/twitter/*")
+@RequestMapping(value = "/app/twitter/*")
 public class SpringTwitterController {
-	
-	Logger log = LoggerFactory.getLogger(getClass());
-	
-	@Autowired SecurityService securityService;
-	
-	@RequestMapping
-	public ModelAndView requestAuthorization() throws TwitterException {
-		
-		SpringTwitterAuthorization st = new SpringTwitterAuthorization();
-		String authURL = "";
-		
-		if (st.checkAuthorization(securityService)) {
-			st.getAuthorization(securityService);
-			return new ModelAndView(JSON_VIEW).addObject("auth",true).addObject("success",true);
-		} else {
-			authURL = st.getAuthorizationURL(securityService);
-			return new ModelAndView(JSON_VIEW).addObject("auth",false).addObject("success",true).addObject("authURL", authURL);
-		}
-	}
-	
-	@RequestMapping(method = RequestMethod.GET)
-    public String getAccessToken(@RequestParam String oauth_token, Model model){
-		SpringTwitterAuthorization st = new SpringTwitterAuthorization();
-		Boolean result = false;
-		
-		try {
-			result = st.storeAuthToken(oauth_token, securityService);
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		model.addAttribute("result", result);
-		
-		return "/springtwitter/getToken";
+
+    Logger log = LoggerFactory.getLogger(getClass());
+    @Autowired
+    SpringTwitterAuthorization springTwitterAuthorization;
+
+    @RequestMapping
+    public ModelAndView requestAuthorization() {
+
+        String authURL = "";
+
+        if (springTwitterAuthorization.checkAuthorization()) {
+            springTwitterAuthorization.getAuthorization();
+            return new ModelAndView(JSON_VIEW).addObject("auth", true).addObject("success", true);
+        } else {
+            authURL = springTwitterAuthorization.getAuthorizationURL();
+            return new ModelAndView(JSON_VIEW).addObject("auth", false).addObject("success", true).addObject("authURL", authURL);
+        }
     }
-	
-	@RequestMapping
-	public ModelAndView test() {
-		return new ModelAndView(JSON_VIEW).addObject("success",true);
-	}
-	
-	
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getAccessToken(@RequestParam String oauth_token, Model model) {
+        Boolean result = false;
+        result = springTwitterAuthorization.storeAuthToken(oauth_token);
+        model.addAttribute("result", result);
+        return "/springtwitter/getToken";
+    }
+
+    @RequestMapping
+    public ModelAndView test() {
+        return new ModelAndView(JSON_VIEW).addObject("success", true);
+    }
+
+
 }
