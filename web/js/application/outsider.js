ceb7579238851ce6cda1b1288ebfaa582d3fc304
helols <helolsjava@gmail.com>
2010-01-19 10:15:29 +0000

    pirobox 탐색기 추가.
    git-svn-id: http://svn.pentome.com/isyoon/trunk@374 ea5790a9-a2b4-4b5f-b68c-306463eac401

--- web/js/application/outsider.js
+++ web/js/application/outsider.js
@@ -15,7 +15,7 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
     lookup : {},
     init : function() {
         //필요한 JS
-        m31.util.requiredJS("pirobox");
+//        m31.util.requiredJS("pirobox");
     },
 
     createView : function() {
@@ -399,7 +399,8 @@ M31Desktop.SpringSee = Ext.extend(M31.app.Module, {
 
 m31.showImage = function() {
     $().piroBox({
-    	target: 'springsee-win',
+    	target: 'springsee-dataview',
+        eventName:'dblclick',
         my_speed: 600, //animation speed
         bg_alpha: 0.5, //background opacity
         radius: 4, //caption rounded corner
@@ -486,99 +487,6 @@ Ext.extend(ImageDragZone, Ext.dd.DragZone, {
         return false;
     }
 });
-
-// 멀티셀렉트
-Ext.DataView.DragSelector = function(cfg) {
-    cfg = cfg || {};
-    var view, proxy, tracker;
-    var rs, bodyRegion, dragRegion = new Ext.lib.Region(0, 0, 0, 0);
-    var dragSafe = cfg.dragSafe === true;
-
-    this.init = function(dataView) {
-        view = dataView;
-        view.on('render', onRender);
-    };
-
-    function fillRegions() {
-        rs = [];
-        view.all.each(function(el) {
-            rs[rs.length] = el.getRegion();
-        });
-        bodyRegion = view.el.getRegion();
-    }
-
-    function cancelClick() {
-        return false;
-    }
-
-    function onBeforeStart(e) {
-        return !dragSafe || e.target == view.el.dom;
-    }
-
-    function onStart(e) {
-        view.on('containerclick', cancelClick, view, {single:true});
-        if (!proxy) {
-            proxy = view.el.createChild({cls:'x-view-selector'});
-        } else {
-            if (proxy.dom.parentNode !== view.el.dom) {
-                view.el.dom.appendChild(proxy.dom);
-            }
-            proxy.setDisplayed('block');
-        }
-        fillRegions();
-        view.clearSelections();
-    }
-
-    function onDrag(e) {
-        var startXY = tracker.startXY;
-        var xy = tracker.getXY();
-
-        var x = Math.min(startXY[0], xy[0]);
-        var y = Math.min(startXY[1], xy[1]);
-        var w = Math.abs(startXY[0] - xy[0]);
-        var h = Math.abs(startXY[1] - xy[1]);
-
-        dragRegion.left = x;
-        dragRegion.top = y;
-        dragRegion.right = x + w;
-        dragRegion.bottom = y + h;
-
-        dragRegion.constrainTo(bodyRegion);
-        proxy.setRegion(dragRegion);
-
-        for (var i = 0, len = rs.length; i < len; i++) {
-            var r = rs[i], sel = dragRegion.intersect(r);
-            if (sel && !r.selected) {
-                r.selected = true;
-                view.select(i, true);
-            } else if (!sel && r.selected) {
-                r.selected = false;
-                view.deselect(i);
-            }
-        }
-    }
-
-    function onEnd(e) {
-        if (!Ext.isIE) {
-            view.un('containerclick', cancelClick, view);
-        }
-        if (proxy) {
-            proxy.setDisplayed(false);
-        }
-    }
-
-    function onRender(view) {
-        tracker = new Ext.dd.DragTracker({
-            onBeforeStart: onBeforeStart,
-            onStart: onStart,
-            onDrag: onDrag,
-            onEnd: onEnd
-        });
-        tracker.initEl(view.el);
-    }
-};
-
-
 /*
  ************************
  *	봄트위터
